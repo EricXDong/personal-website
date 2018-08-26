06def68b5dd3139be9329d7b2cf0a273e869861b
eric.dong <eric.dong@hyperloop-one.com>
2018-08-26 13:17:15 -0700

    4 space indents or die

---
+++ src/components/App/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import './app.css';
+
+class App extends React.Component {
+    public render() {
+        return (
+            <div className="sans-serif absolute w-100 h-100 app">
+                <p>das me</p>
+            </div>
+        );
+    }
+}
+
+export default App;
